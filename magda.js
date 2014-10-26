12b24f2da6b895e9020b546cf02b377fa08fdf71
James Fisher <jhf@trifork.com>
2014-10-26 23:43:51 +0000

    prettify

--- magda.js
+++ magda.js
@@ -1,27 +1,27 @@
-(function(){
+(function() {
 
-var counter = 0,
-$items = document.querySelectorAll('.content figure'),
-numItems = $items.length;
+  var counter = 0,
+    $items = document.querySelectorAll('.content figure'),
+    numItems = $items.length;
 
-var showCurrent = function(){
-var itemToShow = Math.abs(counter%numItems);
+  var showCurrent = function() {
+    var itemToShow = Math.abs(counter % numItems);
 
-[].forEach.call( $items, function(el){
-el.classList.remove('show');
-});
+    [].forEach.call($items, function(el) {
+      el.classList.remove('show');
+    });
 
-$items[itemToShow].classList.add('show');
-};
+    $items[itemToShow].classList.add('show');
+  };
 
-document.querySelector('.next').addEventListener('click', function() {
-counter++;
-showCurrent();
-}, false);
+  document.querySelector('.next').addEventListener('click', function() {
+    counter++;
+    showCurrent();
+  }, false);
 
-document.querySelector('.prev').addEventListener('click', function() {
-counter--;
-showCurrent();
-}, false);
+  document.querySelector('.prev').addEventListener('click', function() {
+    counter--;
+    showCurrent();
+  }, false);
 
 })();
